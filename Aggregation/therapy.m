915756c149681da121d827128f029dd5e526d869
WimLogister <wimlogister@hotmail.com>
2015-04-25 13:32:08 +0200

    Cleaned up therapy file a lot: plotting has been improved, but most importantly I am now using a structure array instead of global variables. Will continue cleaning up dosedyn file a bit more.

--- Aggregation/therapy.m
+++ Aggregation/therapy.m
@@ -7,70 +7,57 @@ bval=5; % Effectiveness of resistance
 mval=0.1; % Chemotherapy dosage (paper says 0.1 for monotherapy)
 sval=0.001; % Evolutionary speed
 
-% System parameters
+% Aggregation parameters
 alphaval=0; % Power to determine the type of aggregation effect
 betaval=0; % Scaling factor for other tumor cells' resistance
 Nval=1; % Neighbourhood size
 
-% Dilution: alpha = beta = 0
-% Group detoxification: alpha = 1, beta > 0
-% Danger in numbers: alpha = 1.5, beta = 0
-% Group sellout: beta < 0
+params = struct('r',rval,'sig',sigval,'Kmax',Kmaxval,'k',kval,'b',bval,...
+    'm',mval,'s',sval,'alpha',alphaval,'beta',betaval,'N',Nval);
+
+% Model different aggregation effects by setting parameters as follows:
+% 1. Dilution: alpha = beta = 0
+% 2. Group detoxification: alpha = 1, beta > 0
+% 3. Danger in numbers: alpha = 1.5, beta = 0
+% 4. Group sellout: beta < 0
 
 % Declare system input variables
 tumorIni=100; % Initial cancer cell population size
 stratIni=0.0; % Initial phenotypic strategy (resistance) value
-tmax=10000;
-steps=10000;
+tmax=10000; % 
+steps=10000; % Number of integration steps used in ODE solver
 
-% Lengths of treatment and rest periods
-treat_length=100;
-rest_length=2000;
+% Constant, uniform treatment
+treat0 = @(t) 1;
 
+% Low-level constant treatment followed by longer, more intense constant treatment
+treat1 = @(t) 0.25*(t >= 0 & t <= 2500) + ...
+    1.25*( t >= 2501 & t <= 10000);
 
-low_start = 0;
-low_end = 2500;
-
-high_start = 2501;
-high_end = 10000;
-
-
-treat = @(t) 0.25*(t >= low_start & t <= low_end) + ...
-    1.25*( t >= high_start & t <= high_end);
-
-period=2500;
-
-treat = @(t)0.4*(t >= 0 & t < 2500) + 0.8*(t >= 2500 & t < 5000) ...
+% Increasingly intense constant treatment
+treat2 = @(t)0.4*(t >= 0 & t < 2500) + 0.8*(t >= 2500 & t < 5000) ...
     + 1.2*(t >= 5000 & t < 7500) + 1.6*(t >= 7501 & t < 10000);
 
-%treat= @(t) 1;
-
-% Treatment bookkeeping variables
-treatmentval=1;
-indexval=1;
-schedule=treat_sched(steps,treat_length,rest_length);
-schedule=[250 1000 1250 2000 2250 3000 3250 4000 4250 5000 5250 6000 6250 7000 7250 8000 8250 9000 9250 10000];
-schedule=[500 2500 3000 5000 5500 7500 8000 9000 9500 10000];
-%schedule=[2500 10000];
-%schedule=[1000 4000 5000 8500 9000 10000];
-%schedule=[tmax];
-
-% Declare constants and parameters as global
-setGlobalParams(rval,sigval,alphaval,Nval,kval,bval,betaval,mval,Kmaxval,...
-    sval,indexval,treatmentval,schedule);
-
 % Solve the ODE system
-[T,X] = ode45(@(t,x) dosedyn(t,x,treat(t)),0:tmax/steps:tmax,[tumorIni stratIni]);
-%[T,X] = ode45(@(t,x) aggdyn(t,x),[0 tmax],[tumorIni stratIni]);
+[T,X] = ode45(@(t,x) dosedyn(t,x,treat1(t),params),0:tmax/steps:tmax,[tumorIni stratIni]);
 
-muX=mean(X(:,1))
-muU=mean(X(:,2))
+muX=mean(X(:,1)) % Average tumor population size
+muU=mean(X(:,2)) % Average resistance strategy value
+
+x_label = sprintf('mu_{X} = %.3f',muX);
+u_label = sprintf('mu_{u} = %.3f',muU);
 
 % Plot results
 figure(1)
-plot(T,X(:,1),'r')
-axis([0 tmax 0 100]);
+subplot(211),plot(T,X(:,1),'r')
+hold on
+subplot(211),line([0 tmax], [muX muX], 'Color', 'k')
+hold off
+legend('population density'),axis([0 tmax 0 100])
 title('population density')
-figure(2)
-plot(T,X(:,2),'b');
-title('average resistance strategy')
+text(8000,muX+10,x_label)
+
+subplot(212),plot(T,X(:,2),'b'),line([0 tmax], [muU muU], 'Color', 'k')
+legend('resistance value', 'Location', 'northwest')
+title('resistance strategy value')
+text(8000,muU+max(X(:,2))/10,u_label)
