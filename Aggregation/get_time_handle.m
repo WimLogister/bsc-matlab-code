523cd3be473cbcb5ddf3478b65bcedaf4fe79438
WimLogister <wimlogister@hotmail.com>
2015-05-07 20:43:31 +0200

    Removed some files that I'm not using anymore.

--- Aggregation/get_time_handle.m
+++
@@ -1,6 +0,0 @@
-function h = get_time_handle( table )
-h = @get_time_at;
-    function time = get_time_at( t )
-        time = table(t);
-    end
-end
--- Aggregation/step.m
+++
@@ -1,8 +0,0 @@
-function [y]=krok(t,x,h)
-% Runge-kutta method to discretize the ODE system (you can use this literally, except of that your expressions won't be vectors, but scalars)
-k1=h*fde(t,x);
-k2=h*fde(t+h/2,x+k1/2);
-k3=h*fde(t+h/2,x+k2/2);
-k4=h*fde(t+h,x+k3);
-y=x+(k1+2*(k2+k3)+k4)/6;
-
